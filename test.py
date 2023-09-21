7545b4c3f84e6a15e668c2f6229741f61c7f5c14
jinfeng775 <jinfeng775@163.com>
2023-09-21 09:06:35 +0800

    修复bug

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
+
+ip = '49.232.253.214'
+# db.auth("Plated0058","G3hJ6btp7aqaVz")
+useranme = 'Plated0058'
+password = "G3hJ6btp7aqaVz"
 import pymongo
 
-client = pymongo.MongoClient(host='49.232.253.214', port=27017)
+
+client = pymongo.MongoClient(host=ip, port=27017, username=useranme, password=password)
 # db 表示数据库名称，username 用户名 password 密码
-client.db.authenticate("Plated0058","G3hJ6btp7aqaVz")
-
-
-# 建立数据库连接
-db = client.test # or db = client['test']
+db = client['test']
 collection = db['123']
 student = {
-    'id': 1,
-    'name': 'test',
+    'id': 2,
+    'name': '123',
 }
 res = collection.insert_one(student)
 print(res, res.inserted_id)
